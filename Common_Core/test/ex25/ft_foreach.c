0417752721ca2a4a690c9b5ecad5598308f2c50b
Elena Razumovskaia <erazumov@paul-f3Br9s1.clusters.42paris.fr>
2024-11-09 18:49:06 +0100

    Common_Core folder

---
+++ Common_Core/test/ex25/ft_foreach.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_foreach.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 16:45:53 by erazumov          #+#    #+#             */
+/*   Updated: 2024/11/08 19:15:26 by erazumov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_foreach(int *tab, int length, void (*f)(int))
+{
+	int	i;
+
+	i = 0;
+	while (i < length)
+		f(tab[i++]);
+}
+/*
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483647)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = 147483647;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + 48);
+	}
+}
+
+int	main(void)
+{
+	int	arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	ft_foreach(arr, 9, ft_putnbr);
+	ft_putchar('\n');
+	return (0);
+}
+*/
