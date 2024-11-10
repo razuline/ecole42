972087e9a1d2ee5a76cc714e9c6636d6dcbf27ec
Elena Razumovskaia <erazumov@bess-f4r4s2.clusters.42paris.fr>
2024-11-10 12:32:53 +0100

    ft_isalpha done & tested

--- Common_Core/libft_test/ft_isalpha.c
+++ Common_Core/libft_test/ft_isalpha.c
@@ -6,7 +6,41 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:12:32 by erazumov          #+#    #+#             */
-/*   Updated: 2024/11/09 18:12:33 by erazumov         ###   ########.fr       */
+/*   Updated: 2024/11/10 12:24:58 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./libft.h"
+
+int	ft_isalpha(char *str)
+{
+	int	i;
+	int	bool;
+
+	i = 0;
+	bool = 0;
+	if (str[i] == '\0')
+		bool = 1;
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 'A' && str[i] <= 'Z')
+			|| (str[i] >= 'a' && str[i] <= 'z'))
+			bool = 1;
+		else
+			bool = 0;
+		i++;
+	}
+	return (bool);
+}
+/*
+int	main(void)
+{
+	char	s[] = "agf123456";
+
+	if (ft_isalpha(s) == 1)
+		write (1, "Only alphabetical characters or 0", 34);
+	else
+		write (1, "Other characters", 17);
+	return (0);
+}
+*/
--- Common_Core/libft_test/libft.h
+++ Common_Core/libft_test/libft.h
@@ -6,15 +6,44 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:10:42 by erazumov          #+#    #+#             */
-/*   Updated: 2024/11/09 19:00:55 by erazumov         ###   ########.fr       */
+/*   Updated: 2024/11/10 12:27:26 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ctype.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #ifndef FT_LIBFT_H
 # define FT_LIBFT_H
 
+/* Mandatory Part */
+
+//int	ft_isalpha(char *str); /* Alphabethic character test */
+ft_isdigit();
+/* ft_isalnum();
+ft_isascii();
+ft_isprint(); */
+//int	ft_strlen(char *str); /* Finds length of a string */
+/*ft_memset();
+ft_bzero();
+ft_memcpy();
+ft_memmove();
+ft_strlcpy();
+ft_strlcat();
+ft_toupper();
+ft_tolower();
+ft_strchr();
+ft_strrchr();
+ft_strncmp();
+ft_memchr();
+ft_memcmp();
+ft_strnstr();
+ft_atoi();
+ft_calloc();
+ft_strdup();*/
 
 
 #endif
