a866b928f72cf6dae5ed94878beb9f24c3675d19
Elena Razumovskaia <erazumov@bess-f4r4s2.clusters.42paris.fr>
2024-11-10 11:49:43 +0100

    ft_strlen done & tested

--- Common_Core/libft_test/ft_strlen.c
+++ Common_Core/libft_test/ft_strlen.c
@@ -6,7 +6,61 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:14:06 by erazumov          #+#    #+#             */
-/*   Updated: 2024/11/09 18:14:07 by erazumov         ###   ########.fr       */
+/*   Updated: 2024/11/10 11:48:48 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* The ft_strlen() function finds length of a string */
+
+#include "./libft.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+/*
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483647)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = 147483647;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + 48);
+	}
+}
+
+int	main(void)
+{
+	int	len;
+	char	s[] = { "Hello, World!" };
+
+	len = ft_strlen(s);
+	ft_putnbr(len);
+	ft_putchar('\n');
+	return (0);
+}
+*/
--- Common_Core/libft_test/libft.h
+++ Common_Core/libft_test/libft.h
@@ -6,7 +6,15 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:10:42 by erazumov          #+#    #+#             */
-/*   Updated: 2024/11/09 18:10:43 by erazumov         ###   ########.fr       */
+/*   Updated: 2024/11/09 19:00:55 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+#ifndef FT_LIBFT_H
+# define FT_LIBFT_H
+
+
+
+#endif
