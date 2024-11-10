bea07cffc0bbcf30542cb0ace2faeb1df4f13236
Elena Razumovskaia <erazumov@bess-f4r5s11.clusters.42paris.fr>
2024-11-10 19:29:02 +0100

    .

--- Common_Core/libft_test/ft_atoi.c
+++ Common_Core/libft_test/ft_atoi.c
@@ -6,7 +6,88 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:19:28 by erazumov          #+#    #+#             */
-/*   Updated: 2024/11/09 18:19:29 by erazumov         ###   ########.fr       */
+/*   Updated: 2024/11/10 16:39:42 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./libft.h"
+
+int	ft_atoi(char *str);
+
+static int ft_isspace(int c)
+{
+	if (c == ' ' || c == '\f' || c == '\n' || c == '\r'
+		|| c == '\t' || c == '\v')
+		return (1);
+	return (0);	
+}
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	result = 0;
+	sign = 0;
+	i = 0;
+	while (ft_isspace(str[i]) == 1)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = 10 * result + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = 147483648;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int	res;
+	int	res2;
+	
+	if (ac > 0)
+	{
+		res = ft_atoi(av[1]);
+		res2 = atoi(av[1]);
+		ft_putnbr(res);
+		ft_putchar('\n');
+		ft_putnbr(res2);
+	}
+	return (0);
+}
--- Common_Core/libft_test/libft.h
+++ Common_Core/libft_test/libft.h
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:10:42 by erazumov          #+#    #+#             */
-/*   Updated: 2024/11/10 15:53:13 by erazumov         ###   ########.fr       */
+/*   Updated: 2024/11/10 16:31:59 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	ft_isdigit(int c); /* Decimal-digit character test */
 int ft_isalnum(int c); /* Alphanumeric character test */
 int	ft_isascii(int c); /* Test for ASCII character */
 int	ft_isprint(int c); /* Printing character test (space character inclusive) */
-int	ft_strlen(char *str); /* Finds length of a string */
+int	ft_strlen(char *str); /* Find length of a string */
 /*ft_memset();
 ft_bzero();
 ft_memcpy();
@@ -40,9 +40,9 @@ ft_strrchr();
 ft_strncmp();
 ft_memchr();
 ft_memcmp();
-ft_strnstr();
-ft_atoi();
-ft_calloc();
+ft_strnstr();*/
+int	ft_atoi(char *str); /* Convert ASCII string to integer */
+/*ft_calloc();
 ft_strdup();*/
 
 
