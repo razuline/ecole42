527634d0609864e82ba48d382b3614c7b2c117d1
Elena Razumovskaia <erazumov@bess-f4r4s2.clusters.42paris.fr>
2024-11-10 13:07:32 +0100

    ft_isprint done & tested

--- Common_Core/libft_test/ft_isprint.c
+++ Common_Core/libft_test/ft_isprint.c
@@ -6,7 +6,40 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:14:10 by erazumov          #+#    #+#             */
-/*   Updated: 2024/11/09 18:14:11 by erazumov         ###   ########.fr       */
+/*   Updated: 2024/11/10 13:06:33 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./libft.h"
+
+int	ft_isprint(char *str)
+{
+	int	i;
+	int	bool;
+
+	i = 0;
+	bool = 0;
+	if (str[i] == '\0')
+		bool = 1;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 32 && str[i] < 127)
+			bool = 1;
+		else
+			return (0);
+		i++;
+	}
+	return (bool);
+}
+/*
+int	main(void)
+{
+	char	s[] = {"   asjdgg"};
+	
+	if (ft_isprint(s) == 1)
+		write (1, "Printable", 10);
+	else
+		write (1, "Other characters", 17);
+	return (0);
+}
+*/
--- Common_Core/libft_test/libft.h
+++ Common_Core/libft_test/libft.h
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:10:42 by erazumov          #+#    #+#             */
-/*   Updated: 2024/11/10 12:55:10 by erazumov         ###   ########.fr       */
+/*   Updated: 2024/11/10 13:05:01 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@
 
 /* Mandatory Part */
 
-//int	ft_isalpha(char *str); /* Alphabethic character test */
-//int	ft_isdigit(char *str); /* Decimal-digit character test */
+int	ft_isalpha(char *str); /* Alphabethic character test */
+int	ft_isdigit(char *str); /* Decimal-digit character test */
 /* ft_isalnum();
-ft_isascii();
-ft_isprint(); */
-//int	ft_strlen(char *str); /* Finds length of a string */
+ft_isascii(); */
+int	ft_isprint(char *str); /* Printing character test (space character inclusive) */
+int	ft_strlen(char *str); /* Finds length of a string */
 /*ft_memset();
 ft_bzero();
 ft_memcpy();
