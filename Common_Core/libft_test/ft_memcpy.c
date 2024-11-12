46520f6fc1e186c86390a10a8e889e70fcbd3372
Elena Razumovskaia <erazumov@paul-f5Br4s1.clusters.42paris.fr>
2024-11-12 18:47:59 +0100

    ft_memcpy done

--- Common_Core/libft_test/ft_memcpy.c
+++ Common_Core/libft_test/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:15:02 by erazumov          #+#    #+#             */
-/*   Updated: 2024/11/12 18:11:24 by erazumov         ###   ########.fr       */
+/*   Updated: 2024/11/12 18:46:52 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,41 @@ void	*ft_memcpy(void *dest, void *src, size_t n)
 	n_dest = (char *)dest;
 	n_src = (char *)src;
 	i = 0;
-	while (i < n)
+	if (!n_dest && !n_src)
+		return (NULL);
+	if (n_dest != n_src)
 	{
-		n_dest[i] = n_src[i];
-		i++;
+		while (i < n)
+		{
+			n_dest[i] = n_src[i];
+			i++;
+		}
 	}
+	return (dest);
+}
+/*
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*(str + i))
+		i++;
+	write (1, str, i);
 }
 
 int	main(void)
 {
-	
+	char src1[] = "Hello, World";
+	char dest1[30];
+
+	ft_memcpy(dest1, src1, ft_strlen(src1) + 1);
+	ft_putstr(dest1);
+	return (0);
 }
+*/
