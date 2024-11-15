e851820fff381f0ea75d1310c04ca02b37d2f759
Elena Razumovskaia <erazumov@paul-f4Br1s1.clusters.42paris.fr>
2024-11-15 19:23:45 +0100

    ft_itoa done

--- Common_Core/libft_test/ft_itoa.c
+++ Common_Core/libft_test/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 13:49:22 by erazumov          #+#    #+#             */
-/*   Updated: 2024/11/15 15:40:28 by erazumov         ###   ########.fr       */
+/*   Updated: 2024/11/15 19:23:23 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static int	ft_intlen(int nb)
 {
 	int	len;
-	
+
 	if (nb == -2147483648)
 	{
 		len = 11;
@@ -27,41 +27,55 @@ static int	ft_intlen(int nb)
 		nb *= -1;
 		len++;
 	}
-	if (nb <= 10)
-		len++;
 	while (nb > 9)
 	{
 		nb /= 10;
 		len++;
 	}
+	if (nb < 10)
+		len++;
 	return (len);
-}
-
-char	*ft_inttostr(int nbr, int len)
-{
-	
 }
 
 char	*ft_itoa(int num)
 {
+	int		len;
 	char	*str;
-	
-	str = (char *)malloc(sizeof(char) * (ft_intlen(num) + 1));
-	if (str == NULL)
-		return (NULL);
-	if (!num)
-		return (str = '\0');
-}
+	long	number;
 
+	len = ft_intlen(num);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	number = num;
+	if (number == 0)
+		str[0] = '0';
+	if (number < 0)
+	{
+		str[0] = '-';
+		number = -number;
+	}
+	while (number != 0)
+	{
+		str[len - 1] = (number % 10) + '0';
+		number /= 10;
+		len--;
+	}
+	return (str);
+}
+/*
 #include <stdio.h>
 
 int	main(void)
 {
-	int	i;
-	int	res;
+	int		i;
+	char	*res;
 
-	i = -10;
-	res = ft_intlen(i);
-	printf("%d\n", res);
+	i = 10;
+	printf("%d\n", ft_intlen(i));
+	res = ft_itoa(i);
+	printf("%s\n", res);
 	return(0);
 }
+*/
