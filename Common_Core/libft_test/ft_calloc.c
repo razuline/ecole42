40b3244a8a9831b26d0225d2155a2580add432c6
Elena Razumovskaia <erazumov@paul-f5Br6s6.clusters.42paris.fr>
2024-11-21 18:57:15 +0100

    All

--- Common_Core/libft_test/ft_calloc.c
+++ Common_Core/libft_test/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:20:10 by erazumov          #+#    #+#             */
-/*   Updated: 2024/11/15 19:26:12 by erazumov         ###   ########.fr       */
+/*   Updated: 2024/11/21 18:25:17 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,4 @@ void	*ft_calloc(size_t nmemb, size_t size)
 		return (0);
 	ft_bzero(result, nmemb * size);
 	return (result);
-}
-
-int	main(void)
-{
-	
 }
--- Common_Core/libft_test/libft.h
+++ Common_Core/libft_test/libft.h
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:10:42 by erazumov          #+#    #+#             */
-/*   Updated: 2024/11/16 15:53:22 by erazumov         ###   ########.fr       */
+/*   Updated: 2024/11/21 18:42:43 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,38 +21,38 @@
 
 /* Mandatory Part */
 
-int		ft_isalpha(int c); /* Alphabethic character test */
-int		ft_isdigit(int c); /* Decimal-digit character test */
-int		ft_isalnum(int c); /* Alphanumeric character test */
-int		ft_isascii(int c); /* Test for ASCII character */
-int		ft_isprint(int c); /* Printing character test (space character inclusive) */
-int		ft_strlen(char *str); /* Find length of a string */
-void	*ft_memset(void *dest, int c, size_t len); /* Fill memory with a constant byte */
-void	ft_bzero(void *dest, size_t len); /* Zero a byte string */
-void	*ft_memcpy(void *dest, void *src, size_t n); /* Copy memory area */
-void	*ft_memmove(void *dest, void *src, size_t len); /* Copy memory area */
-size_t	ft_strlcpy(char *dest, char *src, size_t size); /* Size-bounded string copying */
-int		ft_strlcat(char *dest, char *src, int size); /* Size-bounded string concatenation */
-char	*ft_toupper(char *str); /* Lowercase to uppercase letter conversion*/
-char	*ft_tolower(char *str); /* Uppercase to lowercase letter conversion*/
-char	*ft_strchr(const char *s, int c); /* Locate character in string (first occurence) */
-char	*ft_strrchr(const char *s, int c); /* Locate character in string (last occurence) */
-int		ft_strncmp(char *s1, char *s2, size_t n); /* Compare strings (size-bounded) */
-void	*ft_memchr(void *s, int c, size_t len); /* Scan memory for a character */
-int		ft_memcmp(void *s1, void *s2, size_t len); /* Compare memory areas */
-char	*ft_strnstr(char *big, char *little, int len); /* Locate a substring in a string (size-bounded) */
-int		ft_atoi(char *str); /* Convert ASCII string to integer */
-void	*ft_calloc(size_t nmemb, size_t size); /* Memory allocation */
-char	*ft_strdup(char *src); /* Save a copy of a string (with malloc) */
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(char *str);
+void	*ft_memset(void *dest, int c, size_t len);
+void	ft_bzero(void *dest, size_t len);
+void	*ft_memcpy(void *dest, void *src, size_t n);
+void	*ft_memmove(void *dest, void *src, size_t len);
+size_t	ft_strlcpy(char *dest, char *src, size_t size);
+int		ft_strlcat(char *dest, char *src, int size);
+char	*ft_toupper(char *str);
+char	*ft_tolower(char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(char *s1, char *s2, size_t n);
+void	*ft_memchr(void *s, int c, size_t len);
+int		ft_memcmp(void *s1, void *s2, size_t len);
+char	*ft_strnstr(char *big, char *little, int len);
+int		ft_atoi(char *str);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(char *src);
 
 /* Additional Functions */
 
-char	*ft_substr(char *s, int start, int len); /* Allocate and return a substring */
-char	*ft_strjoin(char *s1, char *s2); /* Allocate and return a new string */
-char	*ft_strtrim(char *s1, char *s2); /* Allocate and return a copy of string */
-char	**ft_split(char *str, char sep); /* Allocate and return an array of strings */
-char	*ft_itoa(int num); /* Return a string representing the integer */
-char	*ft_strmapi(char *s, char (*f)(unsigned int, char)); /* Create a new string */
+char	*ft_substr(char *s, int start, int len);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strtrim(char *s1, char *s2);
+char	**ft_split(char *str, char sep);
+char	*ft_itoa(int num);
+char	*ft_strmapi(char *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
@@ -61,7 +61,7 @@ void	ft_putnbr_fd(int nb, int fd);
 
 /* Bonus Part */
 
-typedef struct	s_list
+typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
