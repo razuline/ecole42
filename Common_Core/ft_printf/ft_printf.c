f673bc0ff605e0c357bd2c14f86b350de1830026
Elena Razumovskaia <erazumov@paul-f5Br4s1.clusters.42paris.fr>
2024-11-25 16:00:21 +0100

    First functions

---
+++ Common_Core/ft_printf/ft_printf.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 14:50:42 by erazumov          #+#    #+#             */
+/*   Updated: 2024/11/25 15:57:23 by erazumov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+int	ft_printf(const char *format, ...)
+{
+	int		numofchar;
+	va_list	arg_list;
+
+	va_start(arg_list, format);
+
+	numofchar = 0;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+	format++;
+			if (*format == '%')
+			{
+				if (*format == '%')
+					write(1, '%', 1);
+			}
+		}
+	}
+}
---
+++ Common_Core/ft_printf/printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 14:16:37 by erazumov          #+#    #+#             */
+/*   Updated: 2024/11/25 15:11:16 by erazumov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+
+# include <ctype.h>
+# include <fcntl.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+int		ft_printf(const char *format, ...);
+
+void	ft_print_char(char c);
+
+
+#endif
