a6e59208db4adc8211680ceac9312c486b5d236e
Elena Razumovskaia <erazumov@paul-f5Br3s1.clusters.42paris.fr>
2024-11-25 16:12:20 +0100

    All changes

--- Common_Core/ft_printf/ft_printf.c
+++ Common_Core/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 14:50:42 by erazumov          #+#    #+#             */
-/*   Updated: 2024/11/25 15:57:23 by erazumov         ###   ########.fr       */
+/*   Updated: 2024/11/25 16:04:58 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,8 @@ int	ft_printf(const char *format, ...)
 {
 	int		numofchar;
 	va_list	arg_list;
-
 	va_start(arg_list, format);
-
+	
 	numofchar = 0;
 	while (*format)
 	{
